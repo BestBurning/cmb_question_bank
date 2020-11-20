9e86a77fa85cf60913cae7fbec4d57040766cba1
Shea <zhushuai026@gmail.com>
2020-11-20 16:27:15 +0800

    init

---
+++ lib/view/question_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class QuestionPage extends StatefulWidget {
+  @override
+  _QuestionPageState createState() {
+    return _QuestionPageState();
+  }
+}
+
+class _QuestionPageState extends State<QuestionPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Text("问题");
+  }
+}
